db99302fa59e0e511469108e464ae9ec5a2ce639
Anastasia Orlova <metkusblackus@gmail.com>
2020-04-28 16:15:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,8 +109,8 @@ def changing_alan
       }
     }
 
-programmer_hash [:alan_kay][:known_for] = "GUI"
-programmer_hash
+alans_new_info = "GUI"
+programmer_hash [:alan_kay][:known_for] = alans_new_info
 end
 
 def adding_to_dennis
